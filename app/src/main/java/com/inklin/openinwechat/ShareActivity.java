7a6f018f78a0033651c538bdd58ab3d957a8b753
acaoairy <acaoairy@live.cn>
2017-09-11 09:08:33 +0800

    缩减了体积

--- app/src/main/java/com/inklin/openinwechat/ShareActivity.java
+++ app/src/main/java/com/inklin/openinwechat/ShareActivity.java
@@ -9,7 +9,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;

941aff66a2cfc0a552f1dec54fc8d6f64a3fee8a
acaoairy <acaoairy@live.cn>
2017-09-14 23:25:49 +0800

    增加支付宝schema

--- app/src/main/java/com/inklin/openinwechat/ShareActivity.java
+++ app/src/main/java/com/inklin/openinwechat/ShareActivity.java
@@ -1,36 +1,11 @@
 package com.inklin.openinwechat;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
 import com.inklin.openinwechat.utils.PreferencesUtils;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,8 +34,9 @@ public class ShareActivity extends Activity {
         }
         if(Intent.ACTION_VIEW.equals(action)){
             Uri uri = intent.getData();
-            if(uri != null)
+            if(uri != null){
                 openInWechat(uri.toString());
+            }
         }
         this.finish();
     }
@@ -84,52 +60,5 @@ public class ShareActivity extends Activity {
             Intent intent = new Intent(this, PreferencesActivity.class);
             startActivity(intent);
         }
-        /*
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.v("URL", url);
-
-                try {
-                    Class<?> personClazz02 = Class.forName("Person");
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                execRootCmdSilent("am start -n com.tencent.mm/com.tencent.mm.plugin.scanner.ui.BaseScanUI -a android.intent.action.VIEW -d '" + url + "'");
-                //execRootCmdSilent("am start -S -n com.tencent.mm/com.tencent.mm.plugin.webview.ui.tools.WebViewUI --es rawUrl '" + url + "'");
-            }
-        }).start();
-        */
     }
-/*
-    // 执行命令但不关注结果输出
-    public static int execRootCmdSilent(String cmd) {
-        int result = -1;
-        DataOutputStream dos = null;
-
-        try {
-            Process p = Runtime.getRuntime().exec("su");
-            dos = new DataOutputStream(p.getOutputStream());
-
-            dos.writeBytes(cmd + "\n");
-            dos.flush();
-            dos.writeBytes("exit\n");
-            dos.flush();
-            p.waitFor();
-            result = p.exitValue();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (dos != null) {
-                try {
-                    dos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return result;
-    }
-    */
 }

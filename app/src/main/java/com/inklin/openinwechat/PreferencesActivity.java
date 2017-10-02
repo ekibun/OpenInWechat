521ea7fc53fd8762cb4111b5bba3aca196a0437a
acaoairy <acaoairy@live.cn>
2017-10-02 13:19:29 +0800

    增加小黄车schema

--- app/src/main/java/com/inklin/openinwechat/PreferencesActivity.java
+++ app/src/main/java/com/inklin/openinwechat/PreferencesActivity.java
@@ -113,8 +113,7 @@ public class PreferencesActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String intentFullUrl = "intent://platformapi/startapp?saId=10000007&" +
-                        "clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2FFKX04432XWNQIFV2UDCR64%3F_s" +
-                        "%3Dweb-other&_t=1472443966571#Intent;" +
+                        "qrcode=https%3A%2F%2Fqr.alipay.com%2FFKX04432XWNQIFV2UDCR64#Intent;" +
                         "scheme=alipayqr;package=com.eg.android.AlipayGphone;end";
                 try {
                     Intent intent = Intent.parseUri(intentFullUrl, Intent.URI_INTENT_SCHEME );
--- app/src/main/java/com/inklin/openinwechat/ShareActivity.java
+++ app/src/main/java/com/inklin/openinwechat/ShareActivity.java
@@ -2,10 +2,14 @@ package com.inklin.openinwechat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
 import com.inklin.openinwechat.utils.PreferencesUtils;
 
+import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,7 +18,7 @@ public class ShareActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.load_layout);
+        //setContentView(R.layout.load_layout);
 
         Intent intent = getIntent();
         String action = intent.getAction();
@@ -27,7 +31,8 @@ public class ShareActivity extends Activity {
                     Matcher matcher = Pattern.compile("(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]").matcher(sharedText);
                     if (matcher.find()) {
                         openInWechat(matcher.group());
-                        return;
+                    }else{
+                        openInWechat(sharedText);
                     }
                 }
             }
@@ -35,7 +40,40 @@ public class ShareActivity extends Activity {
         if(Intent.ACTION_VIEW.equals(action)){
             Uri uri = intent.getData();
             if(uri != null){
-                openInWechat(uri.toString());
+                if("qr.alipay.com".equals(uri.getHost().toLowerCase())) {
+                    String u = uri.toString().replace("QR.ALIPAY.COM", "qr.alipay.com");
+                    Matcher matcher = Pattern.compile("qr.alipay.com/(.+)").matcher(u);
+                    if (matcher.find()) {
+                        String intentFullUrl = "intent://platformapi/startapp?saId=10000007&" +
+                                "qrcode=https%3A%2F%2Fqr.alipay.com%2F" + matcher.group(1) + "#Intent;" +
+                                "scheme=alipayqr;package=com.eg.android.AlipayGphone;end";
+                        try {
+                            Intent intentAli = Intent.parseUri(intentFullUrl, Intent.URI_INTENT_SCHEME);
+                            startActivity(intentAli);
+                        } catch (URISyntaxException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else if("ofo.so".equals(uri.getHost())) {
+                    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+                    if(sp.getBoolean("ofo_wechat", false))
+                        openInWechat(uri.toString());
+                    else{
+                        Matcher matcher = Pattern.compile("ofo.so/plate/(.+)").matcher(uri.toString());
+                        if (matcher.find()) {
+                            String intentFullUrl = "intent://platformapi/startapp?saId=10000007&" +
+                                    "clientVersion=3.7.0.0718&qrcode=http%3A%2F%2Fofo.so%2Fplate%2F" + matcher.group(1) + "#Intent;" +
+                                    "scheme=alipayqr;package=com.eg.android.AlipayGphone;end";
+                            try {
+                                Intent intentAli = Intent.parseUri(intentFullUrl, Intent.URI_INTENT_SCHEME);
+                                startActivity(intentAli);
+                            } catch (URISyntaxException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }else
+                    openInWechat(uri.toString());
             }
         }
         this.finish();
